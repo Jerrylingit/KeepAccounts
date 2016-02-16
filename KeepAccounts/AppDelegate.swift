ee32e28553b592951316a60852acd2d250858282
Jerry <lrlfresh@163.com>
2016-02-16 08:33:48 +0800

    remove the storyboard

--- KeepAccounts/AppDelegate.swift
+++ KeepAccounts/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let chooseItem = ViewController()
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = chooseItem
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- KeepAccounts/ViewController.swift
+++ KeepAccounts/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
