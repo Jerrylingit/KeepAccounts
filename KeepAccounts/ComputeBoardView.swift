7897af1a1f9d631e7662eb9993fbe7c6c84c72e2
Jerry <lrlfresh@163.com>
2016-02-16 20:02:45 +0800

    project

---
+++ KeepAccounts/ComputeBoardView.swift
@@ -0,0 +1,23 @@
+//
+//  ComputeBoardView.swift
+//  KeepAccounts
+//
+//  Created by admin on 16/2/16.
+//  Copyright © 2016年 jerry. All rights reserved.
+//
+
+import UIKit
+
+class ComputeBoardView: UIView {
+    
+    //自定义初始化方法
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
