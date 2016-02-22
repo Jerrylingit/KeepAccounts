df215586707131390acd80fa4d41ae79c4c53a9b
Jerry <lrlfresh@163.com>
2016-02-22 20:54:16 +0800

    add SingleAccountView

---
+++ KeepAccounts/KAMainController/SingleAccountView/SingleAccountView.swift
@@ -0,0 +1,26 @@
+//
+//  SingleAccountView.swift
+//  KeepAccounts
+//
+//  Created by admin on 16/2/22.
+//  Copyright © 2016年 jerry. All rights reserved.
+//
+
+import UIKit
+
+class SingleAccountView: UIView {
+
+    //自定义初始化方法
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    func setup(){
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
